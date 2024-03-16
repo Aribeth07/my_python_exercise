3661e3e6aa4be515d1574c936a1190f6d08224b4
Aribeth07 <tomorrow1070@hotmail.com>
2024-03-16 10:17:21 +0800

    add new

---
+++ yen.py
@@ -0,0 +1,21 @@
+def main():
+    x = int(input("How much Yen? "))
+    print("Dollar algorithm:", dollar(x))
+    print("Simple algorithm:", simp(x))
+    print("Diff: ", dollar(x) - simp(x))
+    print("The real value: ", real(x))
+    print("Dollar algorithm bias: ", dollar(x)-real(x))
+    print("Simple algorithm bias: ", simp(x)-real(x))
+
+def dollar(n):
+    return 7 * n / 150
+
+def simp(n):
+    return n / 20
+
+def real(n):
+    return n / 20.41
+
+while True:
+    if __name__ == "__main__":
+        main()
