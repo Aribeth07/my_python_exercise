22cba02c66c83070cd100237c66588b1aafb8c5c
Aribeth07 <tomorrow1070@hotmail.com>
2024-03-16 10:18:37 +0800

    no prefix

--- baseConv.py
+++ baseConv.py
@@ -19,34 +19,31 @@ def hex_num(x):
     dec_result = int(x,16)
     return bin_result, oct_result, dec_result
 
-     
-
 while True:
-    n = input("number? ")
-    if n.startswith("0b", 0, 2):
-        d, o, h = bin_num(n)
-        o = o.strip("0o")
-        h = h.strip("0x")
-        print(f"--------------------\nDecimal: {d}\nOctal: {o}\nHexadecimal: {h}\n--------------------")
-    elif n.startswith("0o", 0, 2):
-        b, d, h = oct_num(n)
-        b = b.strip("0b")
-        h = h.strip("0x")
-        print(f"--------------------\nBinary: {b}\nDecimal: {d}\nHexadecimal: {h}\n--------------------")
-    elif n.startswith("0x", 0, 2):
-        b, o, d = hex_num(n)
-        b = b.strip("0b")
-        o = o.strip("0o")
-        print(f"--------------------\nBinary: {b}\nOctal: {o}\nDecimal: {d}\n--------------------")
-    elif n.isnumeric():
-        b, o, h = dec_num(n)
-        b = b.strip("0b")
-        o = o.strip("0o")
-        h = h.strip("0x")
-        print(f"--------------------\nBinary: {b}\nOctal: {o}\nHexadecimal: {h}\n--------------------")
-        #print(dec_num(int(n)))
-    else: 
-        print("Invalid number. Try again.")
-    dasu = input("Another?(Y/n) ")
-    if dasu == "n":
+    n = input("Enter a positive integer\nTo convert non-decimal numbers, add prefixes(0b, 0o, 0x)\nType \"q\" to quit:")
+    if n == "q":
         break
+    else:
+        if n.startswith("0b", 0, 2):
+            d, o, h = bin_num(n)
+            o = o.strip("0o")
+            h = h.strip("0x")
+            print(f"--------------------\nDecimal: {d}\nOctal: {o}\nHexadecimal: {h}\n--------------------")
+        elif n.startswith("0o", 0, 2):
+            b, d, h = oct_num(n)
+            b = b.strip("0b")
+            h = h.strip("0x")
+            print(f"--------------------\nBinary: {b}\nDecimal: {d}\nHexadecimal: {h}\n--------------------")
+        elif n.startswith("0x", 0, 2):
+            b, o, d = hex_num(n)
+            b = b.strip("0b")
+            o = o.strip("0o")
+            print(f"--------------------\nBinary: {b}\nOctal: {o}\nDecimal: {d}\n--------------------")
+        elif n.isnumeric():
+            b, o, h = dec_num(n)
+            b = b.strip("0b")
+            o = o.strip("0o")
+            h = h.strip("0x")
+            print(f"--------------------\nBinary: {b}\nOctal: {o}\nHexadecimal: {h}\n--------------------")
+        else: 
+            print("Invalid number.")
